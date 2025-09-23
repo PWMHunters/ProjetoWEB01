9ba6a866c24bbae32648cf52f2da881a983c1953
João Vitor <99694870+joaovcguerra@users.noreply.github.com>
2025-09-23 02:32:47 -0300

    correção home head

--- my-anime-review/src/app/page.js
+++ my-anime-review/src/app/page.js
@@ -3,10 +3,8 @@
 export default function Home() {
   return (
     <div className="home">
-      <head>
       <title>Avaliador de Animes</title>
       <link rel="shortcut icon" href="/images/guia.png" />
-      </head>
       <h2>Bem-vindo ao Avaliador de Animes!</h2>
       <p>Use a barra de busca acima para encontrar um anime.</p>
     </div>
